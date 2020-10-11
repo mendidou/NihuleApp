6dc1c120c17caa77ac41fc27a6c98c33780cbbb9
mendy <maouizerat@gmail.com>
2020-10-11 16:48:27 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.post('/register', function(req, res, next) {
   const email = req.body.email
   const password = req.body.password
    const SQL = "INSERT INTO Users (email,password) VALUES ($1 ,$2)"
-   pool.query(SQL , [email,password], function(dbError , dbResult) {
+   pool.query(SQL , ["mendy","password"], function(dbError , dbResult) {
 
     if(dbError){
       res.json(dbError)
