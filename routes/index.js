39171ef078065343c350d7bb163f5b640e67de9a
mendy <maouizerat@gmail.com>
2020-10-11 21:37:23 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -22,11 +22,13 @@ const pool = new Pool({
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+  
 
 });
 
 /* get all users */
-router.get('/users', function (req, res, next) {
+router.get('/users',  function (req, res, next) {
+  
   const SQL = "SELECT * FROM Users";
   pool.query(SQL, [], function (dbError, dbResult) {
     if (dbError) {
@@ -39,19 +41,30 @@ router.get('/users', function (req, res, next) {
 
 
 /* register a  user to the database */
-router.post('/register', function (req, res, next) {
+router.post('/register', async function (req, res, next) {
+  try{
+    const password = req.body.password
+    const email = req.body.email
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(password ,salt)
+    console.log(salt)
+    console.log(hashedPassword)
+    console.log(password)
+    const SQL = "INSERT INTO Users(email,password) VALUES( $1 , $2)"
+    pool.query(SQL, [email, hashedPassword], function (dbError, dbResult) {
   
-  const email = req.body.email
-  const password = req.body.password
-  const SQL = "INSERT INTO Users(email,password) VALUES( $1 , $2)"
-  pool.query(SQL, [email, password], function (dbError, dbResult) {
+      if (dbError) {
+        res.json(dbError)
+        return
+      }
+      res.json(dbResult)
+    })
+  }catch{
+    res.status(500).send
+  }
+  
+  
 
-    if (dbError) {
-      res.json(dbError)
-      return
-    }
-    res.json(dbResult)
-  })
 });
 
 
