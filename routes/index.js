9b48b0a392cbedac0af9a06c95e01bf02d249c04
mendy <maouizerat@gmail.com>
2020-10-11 18:07:12 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,20 @@ const pool = new Pool({
   }
 });
 
+router.post('/register', function(req, res, next){
+  const email = req.body.mail
+  const password = req.body.pass
+   const SQL = "INSERT INTO Users(email,password) VALUES( $1 , $2)"
+   pool.query(SQL , [email,password], function(dbError , dbResult) {
+
+    if(dbError){
+      res.json(dbError)
+      return
+    }
+    res.json(dbResult)
+  })
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   
@@ -27,19 +41,7 @@ router.get('/initdb', function(req, res, next) {
   })
 });
 
-router.post('/register', function(req, res, next){
-  const email = req.body.mail
-  const password = req.body.pass
-   const SQL = "INSERT INTO Users(email,password) VALUES( $1 , $2)"
-   pool.query(SQL , [email,password], function(dbError , dbResult) {
 
-    if(dbError){
-      res.json(dbError)
-      return
-    }
-    res.json(dbResult)
-  })
-});
 
 router.post('/test', function(req ,res ,next) {
   const one = req.body.one
