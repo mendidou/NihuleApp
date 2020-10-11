1843cbf84fc9a941160bd7e56c0d5e78aaaf630b
mendy <maouizerat@gmail.com>
2020-10-11 22:34:41 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,7 @@ router.post('/login', async function (req, res, next) {
     const password = req.body.password
     const email = req.body.email
    
-    const SQL = "SELECT email,password FROM Users WHERE email=maouizerat@gmail.com"
+    const SQL = "SELECT email,password FROM Users WHERE email = 'maouizerat@gmail.com'"
     console.log(SQL)
     pool.query(SQL, [email], function (dbError, dbResult) {
   
