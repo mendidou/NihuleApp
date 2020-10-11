0380ac348e3da9e502f0e40789f4f7a6d07d0990
mendy <maouizerat@gmail.com>
2020-10-11 14:10:11 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { Pool } = require('pg');
 
+const router = express.Router();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/initdb', function(req, res, next) {
+  const SQL = "CREATE TABLE Users(id SERIAL , email TEXT , password TEXT)";
+  pool.query(SQL , [] , function(dbError , dbResult) {
+    if(dbError){
+      res.json(dbError)
+      return
+    }
+    res.json(dbResult)
+  })
+});
+
 
 module.exports = router;
