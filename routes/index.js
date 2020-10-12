e3b9128a2c5f93a0128ebce133ecef9bb847b6c9
mendy <maouizerat@gmail.com>
2020-10-13 00:28:12 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -105,10 +105,10 @@ router.post('/login', async function (req, res, next) {
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.senStatus(401)
+  if (token == null) return res.sendtatus(401)
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.senStatus(403)
+    if (err) return res.sendtatus(403)
     req.user = user
     next()
   })
