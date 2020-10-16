de7b4104c2aa876bb19dc558d629b3b38f50b93f
mendy <maouizerat@gmail.com>
2020-10-16 17:51:27 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -188,6 +188,7 @@ function authenticateToken(req, res, next) {
 //  const token = authHeader && authHeader.split(' ')[1]
 const token = req.cookies.access_token
   if (token == null) return res.sendStatus(401)
+  console.log(token)
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403)
