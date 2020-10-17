fd1f4f75e7835a552d6e910d4ab2d072b1b19c96
mendy <maouizerat@gmail.com>
2020-10-18 02:22:24 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -149,7 +149,7 @@ router.post('/login', async function (req, res, next) {
         const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
 
          const cookie = new Cookie(req ,res ,{})
-         const date = new Date()
+         var date = new Date()
          date = date.setHours(date.getHours + 2).toString
         cookie.set('access_token',accessToken,{signed:false,secure:false,httpOnly:true})
         console.log(date)
