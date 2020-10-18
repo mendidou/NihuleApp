f7b18ec14195920282ec4c0dc9989ab939edf9d0
mendy <maouizerat@gmail.com>
2020-10-18 21:34:51 +0300

    added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+
 var logger = require('morgan');
 
-var authentificationRoutes = require('./routes/authentificationRoutes');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/userRoutes');
 
 var app = express();
@@ -15,10 +16,9 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', authentificationRoutes);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
