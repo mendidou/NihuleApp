84a06e495f696cfe576070f1624e624e38360c10
mendy <maouizerat@gmail.com>
2020-10-18 03:55:22 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -57,20 +57,45 @@ router.get('/', function (req, res, next) {
 
 
 });
+/* check login to the database */
+router.post('/login', async function (req, res, next) {
 
-/* get all users */
-router.get('/users',authenticateToken, function (req, res, next) {
+  try {
+  const password = req.body.password
+  
+  const email = req.body.email
+  const user = { email: email }
+    const SQL = `SELECT * FROM Users WHERE email = $1`
+    pool.query(SQL, [email], async function (dbError, dbResult) {
 
-  const SQL = `SELECT * FROM Users WHERE email = $1`
-  pool.query(SQL, [req.user.email], function (dbError, dbResult) {
-    if (dbError) {
-      res.json(dbError)
-      return
-    }
-    res.json(dbResult)
-  })
+      if (dbError) {
+        res.json(dbError)
+        return
+      }
+      const iscomparable = await bcrypt.compare(password, dbResult.rows[0].password)
+      if (iscomparable) {
+        const accessToken = generateAccessToken(user)
+        const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
+
+         const cookie = new Cookie(req ,res ,{})
+        cookie.set('access_token',accessToken,{signed:false,secure:false,httpOnly:true})
+        cookie.set('refresh_token',refreshToken,{signed:false,secure:false,httpOnly:true})
+      
+   //  res.json({ accessToken: accessToken ,refreshToken:refreshToken })
+    
+       
+        //TODO:  redirect to the dashboard after loged successfull && save the access token in the cookies 
+      } else {
+        return
+        //TODO:redirect to login with a warning message
+      }
+    })
+  } catch{
+    res.status(500).send
+  }
+
+ 
 });
-
 
 /* register a  user to the database */
 router.post('/register', async function (req, res, next) {
@@ -96,125 +121,52 @@ router.post('/register', async function (req, res, next) {
   }
 })
 
-router.get('/show', async function (req, res, next) {
-  
-    const SQL = 'SELECT * from Users'
-    pool.query(SQL, [], function (dbError, dbResult) {
-
-      if (dbError) {
-        res.json(dbError)
-        return
-      }
-      res.json(dbResult)
-    })
-  })
-
-  router.post('/deleteAll'), async function (req, res, next) {
-  
-    const SQL = 'DELETE * from Users'
-    pool.query(SQL, [email, hashedPassword], function (dbError, dbResult) {
-
-      if (dbError) {
-        res.json(dbError)
-        return
-      }
-      res.json(dbResult)
-    })
-  } 
-
-
-
-
-
-
-/* check login to the database */
-router.post('/login', async function (req, res, next) {
-
-  try {
-  const password = req.body.password
-  
-  const email = req.body.email
-  const user = { email: email }
-    const SQL = `SELECT * FROM Users WHERE email = $1`
-    pool.query(SQL, [email], async function (dbError, dbResult) {
-
-      if (dbError) {
-        res.json(dbError)
-        return
-      }
-      
-      const iscomparable = await bcrypt.compare(password, dbResult.rows[0].password)
-      if (iscomparable) {
-        const accessToken = generateAccessToken(user)
-        const refreshToken = jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
-
-         const cookie = new Cookie(req ,res ,{})
-         var date = new Date().toString
-        // date = date.setHours(date.getHours + 2)
-        cookie.set('access_token',accessToken,{signed:false,secure:false,httpOnly:true ,expires:date})
-        console.log(date)
-
-        // const cookie = new Cookie(req , res ,{})
-        // cookie.set('test', 'hello rebek',{signed:false,secure:false,httpOnly:false})
-       //TODO: save refesh token in cookies
-      //  res.cookie('access_token', accessToken ,{
-      //   maxAge:3600,
-      //   httpOnly:false,
-      //   secure:false
-      // })
-      //  res.cookie('refresh_token', refreshToken,{
-      //  maxAge:10000,
-      //  httpOnly:false,
-      //  secure:false
-      //  })
-      // console.log(req.cookies)
-     res.json({ accessToken: accessToken ,refreshToken:refreshToken })
-       // refreshTokens.push(refreshToken)
-       
-        //TODO:  redirect to the dashboard after loged successfull && save the access token in the cookies 
-      } else {
-        return
-        //redirect to login with a warning message
-      }
-    })
-  } catch{
-    res.status(500).send
-  }
-
- 
-});
- 
-router.post('/token', function(req,res,next) {
-  const refreshToken = req.body.token
-  if(refreshToken ==null)return res.sendStatus(401)
-  if(!refreshTokens.includes(refreshToken))return res.sendStatus(403)
-  jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET , (err, user)=>{
-    if(err)return res.sendStatus(403)
-    const accesToken = generateAccessToken({email : user.email})
-    res.json({accesToken:accesToken})
-  })
-})
-
-
-
 router.delete('/logout', function(req,res,next) {
-  refreshTokens = refreshTokens.filter(token => token !==req.body.token)
+  const cookie = new Cookie(req ,res ,{})
+  cookie.set('access_token',"need reconnection",{signed:false,secure:false,httpOnly:true})
+  cookie.set('refresh_token','need reconnection',{signed:false,secure:false,httpOnly:true})
+  //TODO: redirect to login with a small message
   res.sendStatus(204)
 })
 
+/* get all users */
+router.get('/users',authenticateToken, function (req, res, next) {
+
+  const SQL = `SELECT * FROM Users WHERE email = $1`
+  pool.query(SQL, [req.user.email], function (dbError, dbResult) {
+    if (dbError) {
+      res.json(dbError)
+      return
+    }
+    res.json(dbResult)
+  })
+});
 
 
 function authenticateToken(req, res, next) {
   const cookie = new Cookie(req ,res , {})
   const token = cookie.get('access_token',{signed:false})
 
-  if (token == null) return res.sendStatus(401)
-  console.log(token)
+  if (token == null) return //TODO: redirect to login with a small message
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
+    if (err) return req.user = refreshToken(req,res,next)
     req.user = user
     next()
+  })
+}
+
+function refreshToken (req,res,next) {
+  const cookie = new Cookie(req ,res , {})
+  const refreshToken = cookie.get('refresh_token',{signed:false})
+  if(refreshToken ==null)return res.sendStatus(401) //TODO: redirect to login with a small message
+  if(!refreshTokens.includes(refreshToken))return res.sendStatus(403)
+  jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET , (err, user)=>{
+    if(err)return //TODO: redirect to login with a small message
+    const accessToken = generateAccessToken({email : user})
+    cookie.set('access_token',accessToken,{signed:false,secure:false,httpOnly:true})
+    return user
+
   })
 }
  
@@ -222,3 +174,16 @@ function generateAccessToken(user){
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {expiresIn:'1h'})
 }
 module.exports = router;
+
+
+// router.post('/token', function(req,res,next) {
+//   const cookie = new Cookie(req ,res , {})
+//   const refreshToken = cookie.get('refresh_token',{signed:false})
+//   if(refreshToken ==null)return res.sendStatus(401)
+//   if(!refreshTokens.includes(refreshToken))return res.sendStatus(403)
+//   jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET , (err, user)=>{
+//     if(err)return res.sendStatus(403)
+//     const accesToken = generateAccessToken({email : user.email})
+//     newToken = accesToken
+//   })
+// })
