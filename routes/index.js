c303d032a56f9172de4af156a7996e62eb7ed573
mendy <maouizerat@gmail.com>
2020-10-18 21:06:10 +0300

    added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@
 require('dotenv').config({path:`routes/.env`});
 const router = require('./authentification');
 const authMethods = require('./authMethods');
+const app = express();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const { Pool } = require('pg');
+const { token } = require('morgan');
+const Cookie = require('cookies');
 
 
 
