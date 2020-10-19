549c6192d08afb1997f3dfea899d659b33b3d403
mendy <maouizerat@gmail.com>
2020-10-20 01:22:31 +0300

    added

--- routes/routeMethods/authMethods.js
+++ routes/routeMethods/authMethods.js
@@ -22,7 +22,7 @@ const Cookie = require('cookies');
     if(refreshToken ==null)  res.redirect('login',401)  //TODO: redirect to login with a small message
     jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET , (err, user)=>{
       if(err)res.redirect('login' ,403) //TODO: redirect to login with a small message
-      const accessToken = generateAccessToken({email : user})
+      const accessToken = this.generateAccessToken({email : user})
       cookie.set('access_token',accessToken,{signed:false,secure:false,httpOnly:true})
       return user
   
