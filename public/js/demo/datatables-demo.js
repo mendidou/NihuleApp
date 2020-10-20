71575671b5b3d94a6ed9d1ea65a3ef9be0b72bd3
mendy <maouizerat@gmail.com>
2020-10-20 21:33:46 +0300

    added

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,4 +1,20 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  var dataTable = $('#dataTable').DataTable();
+
+   /* Apply the jEditable handlers to the table */
+   dataTable.$('td').editable( '#', {
+    "callback": function( sValue, y ) {
+        var dataPos = dataTable.fnGetPosition( this );
+        dataTable.fnUpdate( sValue, dataPos[0], dataPos[1] );
+    },
+    "submitdata": function ( value, settings ) {
+        return {
+            "row_id": this.parentNode.getAttribute('id'),
+            "column": dataTable.fnGetPosition( this )[2]
+        };
+    },
+    "height": "14px",
+    "width": "100%"
+} );
 });
