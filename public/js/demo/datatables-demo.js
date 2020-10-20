bd9d02a4accc6480b71b42cd7f2dc79e1d671c30
mendy <maouizerat@gmail.com>
2020-10-20 22:22:59 +0300

    added

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,22 +1,70 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  var dataTable = $('#dataTable').DataTable();
-
-   /* Apply the jEditable handlers to the table */
-   dataTable.$('td').editable( '#', {
-    
-    "callback": function( sValue, y ) {
-        var dataPos = dataTable.fnGetPosition( this );
-        dataTable.fnUpdate( sValue, dataPos[0], dataPos[1] );
-    },
-    "submitdata": function ( value, settings ) {
-        return {
-            "row_id": this.parentNode.getAttribute('id'),
-            "column": dataTable.fnGetPosition( this )[2]
-        };
-    },
-    "height": "14px",
-    "width": "100%"
-} );
-console.log("datatable")
+  $('#dataTable').DataTable();
 });
+var editor; // use a global for the submit and return data rendering in the examples
+ 
+$(document).ready(function() {
+    editor = new $.fn.dataTable.Editor( {
+        ajax: "users",
+        table: "#dataTable",
+        fields: [ {
+                label: "First name:",
+                name: "first_name"
+            }, {
+                label: "Last name:",
+                name: "last_name"
+            }, {
+                label: "Position:",
+                name: "position"
+            }, {
+                label: "Office:",
+                name: "office"
+            }, {
+                label: "Extension:",
+                name: "extn"
+            }, {
+                label: "Start date:",
+                name: "start_date",
+                type: "datetime"
+            }, {
+                label: "Salary:",
+                name: "salary"
+            }
+        ]
+    } );
+ 
+    // Activate an inline edit on click of a table cell
+    $('#dataTable').on( 'click', 'tbody td:not(:first-child)', function (e) {
+        editor.inline( this );
+    } );
+ 
+    $('#dataTable').DataTable( {
+        dom: "Bfrtip",
+        ajax: "users",
+        order: [[ 1, 'asc' ]],
+        columns: [
+            {
+                data: null,
+                defaultContent: '',
+                className: 'select-checkbox',
+                orderable: false
+            },
+            { data: "first_name" },
+            { data: "last_name" },
+            { data: "position" },
+            { data: "office" },
+            { data: "start_date" },
+            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
+        ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        buttons: [
+            { extend: "create", editor: editor },
+            { extend: "edit",   editor: editor },
+            { extend: "remove", editor: editor }
+        ]
+    } );
+} );
