fd1ec65baf68a4db208052b0293bd0d08c8b2983
mendy <maouizerat@gmail.com>
2020-10-20 22:36:37 +0300

    added

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,66 +1,18 @@
-var editor; // use a global for the submit and return data rendering in the examples
- 
+// Call the dataTables jQuery plugin
 $(document).ready(function() {
-    editor = new $.fn.dataTable.Editor( {
-        ajax: "users",
-        table: "#dataTable",
-        fields: [ {
-                label: "First name:",
-                name: "first_name"
-            }, {
-                label: "Last name:",
-                name: "last_name"
-            }, {
-                label: "Position:",
-                name: "position"
-            }, {
-                label: "Office:",
-                name: "office"
-            }, {
-                label: "Extension:",
-                name: "extn"
-            }, {
-                label: "Start date:",
-                name: "start_date",
-                type: "datetime"
-            }, {
-                label: "Salary:",
-                name: "salary"
-            }
-        ]
-    } );
- 
-    // Activate an inline edit on click of a table cell
-    $('#dataTable').on( 'click', 'tbody td:not(:first-child)', function (e) {
-        editor.inline( this );
-    } );
- 
-    $('#dataTable').DataTable( {
-        dom: "Bfrtip",
-        ajax: "users",
-        order: [[ 1, 'asc' ]],
-        columns: [
-            {
-                data: null,
-                defaultContent: '',
-                className: 'select-checkbox',
-                orderable: false
-            },
-            { data: "first_name" },
-            { data: "last_name" },
-            { data: "position" },
-            { data: "office" },
-            { data: "start_date" },
-            { data: "salary", render: $.fn.dataTable.render.number( ',', '.', 0, '$' ) }
-        ],
-        select: {
-            style:    'os',
-            selector: 'td:first-child'
-        },
-        buttons: [
-            { extend: "create", editor: editor },
-            { extend: "edit",   editor: editor },
-            { extend: "remove", editor: editor }
-        ]
-    } );
-} );
+  $('#dataTable').DataTable();
+
+  
+    // example.php will be used to send the data to the sever database
+    $('#dataTable').Tabledit({
+    url: 'users',
+    editButton: false,
+    deleteButton: false,
+    hideIdentifier: true,
+    columns: {
+    identifier: [0, 'id'],
+    editable: [[2, 'first'], [3, 'last'],[3, 'nickname']]
+    }
+    });
+    
+});
