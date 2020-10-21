19f4bb526143f9491649375a4635d4b8cc9612f6
mendy <maouizerat@gmail.com>
2020-10-22 00:12:01 +0300

    added

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,18 +1,48 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
+$(document).ready(function () {
   $('#dataTable').DataTable();
 
-  
-    // example.php will be used to send the data to the sever database
-    $('#dataTable').Tabledit({
+
+  // example.php will be used to send the data to the sever database
+  $('#dataTable').Tabledit({
     url: 'login',
-    editButton:true,
+    editButton: true,
     deleteButton: true,
     hideIdentifier: false,
+
     columns: {
-    identifier: [0, 'id'],
-    editable: [[0, 'first'],[1, 'second'],[2, 'third'],[3, 'firth'],[4, 'fiveth'] ]
+      identifier: [0, 'id'],
+      editable: [[0, 'first'], [1, 'second'], [2, 'third'], [3, 'firth'], [4, 'fiveth']]
+    },
+    buttons: {
+      edit: {
+        class: 'btn btn-sm btn-default',
+          html: '<span class="glyphicon glyphicon-pencil"></span>',
+          action: 'edit'
+      },
+      delete: {
+        class: 'btn btn-sm btn-default',
+          html: '<span class="glyphicon glyphicon-trash"></span>',
+            action: 'delete'
+      },
+      save: {
+        class: 'btn btn-sm btn-success',
+          html: 'Save'
+      },
+      restore: {
+        class: 'btn btn-sm btn-warning',
+          html: 'Restore',
+            action: 'restore'
+      },
+      confirm: {
+        class: 'btn btn-sm btn-danger',
+          html: 'Confirm'
+      }
     }
-    });
-    
+
+  });
+
+  
+
+
 });
