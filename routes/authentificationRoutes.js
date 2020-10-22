1aa9393bcd97ab562bf7a0127fcf96c409888bbc
mendy <maouizerat@gmail.com>
2020-10-22 22:08:24 +0300

    added

--- routes/authentificationRoutes.js
+++ routes/authentificationRoutes.js
@@ -88,8 +88,8 @@ router.post('/register', async function (req, res, next) {
       const SQL = 'CREATE TABLE '+ email+'dailyReport (id SERIAL, date DATE, credit FLOAT, debit FLOAT, apt INT, name TEXT, receipt INT, for TEXT, details TEXT,paymenttype TEXT,provider TEXT,differentsprovider TEXT,detailsdiferentProviders TEXT, remarks TEXT)'
       pool.query(SQL,[], function(dbError , dbResult2){
          if(dbError){
-          res.json(dbError.stack + dbError.message)
-          return
+          //res.json(dbError.stack + dbError.message)
+         // return
         }
         res.json(dbResult2)
       })
