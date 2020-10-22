906d985ba5b79307cdd74d7a3a6ad80f2e23705d
mendy <maouizerat@gmail.com>
2020-10-22 20:56:25 +0300

    added

--- routes/authentificationRoutes.js
+++ routes/authentificationRoutes.js
@@ -85,8 +85,8 @@ router.post('/register', async function (req, res, next) {
         res.json(dbError)
         return
       }
-      const SQL = `CREATE TABLE ?? (תאריך DATE , זכות FLOAT, חובה FLOAT, דירה INT , שם TEXT , קבלה INT ,עבור TEXT , פירוט TEXT,סוג תשלום TEXT,ספק TEXT,ספקי שונות TEXT , פירוט ספקי שונות TEXT, הערות TEXT)`
-      pool.query(SQL,[email+"dailyReport"], function(dbError , dbResult2){
+      const SQL = `CREATE TABLE`+email+`dailyReport(תאריך DATE , זכות FLOAT, חובה FLOAT, דירה INT , שם TEXT , קבלה INT ,עבור TEXT , פירוט TEXT,סוג תשלום TEXT,ספק TEXT,ספקי שונות TEXT , פירוט ספקי שונות TEXT, הערות TEXT)`
+      pool.query(SQL,[], function(dbError , dbResult2){
         if(dbError){
           console.log(dbError)
           return
