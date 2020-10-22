d56c79aa3d6221ad1685609550bf285ab8783139
mendy <maouizerat@gmail.com>
2020-10-23 01:15:38 +0300

    added

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,9 +1,7 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
-  $('#dataTable').DataTable();
 
-
-  // example.php will be used to send the data to the sever database
+// example.php will be used to send the data to the sever database
   $('#dataTable').Tabledit({
     url: 'dailyReport',
     editButton: false,
