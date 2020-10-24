1bca2f3f2433a0284ff02397092babfaa499fca1
mendy <maouizerat@gmail.com>
2020-10-25 01:21:23 +0200

    added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -69,8 +69,8 @@ router.post('/addDailyReport',authMethods.data.authenticateToken, function (req,
   });
 
 router.post('/dailyReport',authMethods.data.authenticateToken, function (req, res, next) {
-if(req.body.action == "delete"){
   const dailyReportTable = authMethods.data.dailyReportNameTable(req.user.email)
+if(req.body.action == "delete"){
   const SQL = "DELETE FROM "+dailyReportTable +" WHERE id = $1;"
 
   pool.query(SQL, [req.body.id], function (dbError, dbResult) {
@@ -86,70 +86,64 @@ const editReqs = ['date','credit',  'debit', 'apt',
 'name', 'receipt', 'forsomeone', 'details',
  'paymenttype', 'provider', 'differentsprovider',
 'detailsdiferentProviders',  'remarks']
-var reqStrind = req.body.toString
-var reqBodyJsObject =JSON.parse(reqStrind)
-console.log(reqBodyJsObject)
-res.json("reqBodyJsObject")
-//console.log(reqBodyJsObject)
-//editReqs.forEach(editReq =>{ 
-//  if(reqBodyJsObject[editReq]){
-  //  console.log(reqBodyJsObject[editReq])
-  //  res.json("hello")
+
+if (req.body.date) {
+  const SQL = "UPDATE FROM "+dailyReportTable +" SET date = $1 WHERE id = $2;"
+  pool.query(SQL, [req.body.date,req.body.id], function (dbError, dbResult) {
+    if (dbError) {
+      res.json(dbError.stack)
+      return
+    }
+    res.json(dbResult)
     return
- // }
-  //console.log(1)
-//})
-// if (req.body.date) {
-//   ////
+  })
+}else if (req.body.credit) {
+
+  res.json(req.body.credit)
+}
+else if (req.body.debit) {
+
+  res.json(req.body.debit)
+}
+else if (req.body.apt) {
+
+  res.json(req.body.apt)
+}
+else if (req.body.name) {
+
+  res.json(req.body.two)
+}
+else if (req.body.receipt) {
+  res.json(req.body.two)
+}
+else if (req.body.forsomeone) {
+
+  res.json(req.body.two)
+}
+else if (req.body.details) {
   
-//   res.json(req.body.date)
-// }else if (req.body.credit) {
+  res.json(req.body.two)
+}
+else if (req.body.paymenttype) {
 
-//   res.json(req.body.credit)
-// }
-// else if (req.body.debit) {
+  res.json(req.body.two)
+}
+else if (req.body.provider) {
 
-//   res.json(req.body.debit)
-// }
-// else if (req.body.apt) {
+  res.json(req.body.ten)
+}
+else if (req.body.differentsprovider) {
 
-//   res.json(req.body.apt)
-// }
-// else if (req.body.name) {
+  res.json(req.body.two)
+}
+else if (req.body.detailsdiferentProviders) {
 
-//   res.json(req.body.two)
-// }
-// else if (req.body.receipt) {
-//   res.json(req.body.two)
-// }
-// else if (req.body.forsomeone) {
-
-//   res.json(req.body.two)
-// }
-// else if (req.body.details) {
+  res.json(req.body.two)
+}
+else if (req.body.remarks) {
   
-//   res.json(req.body.two)
-// }
-// else if (req.body.paymenttype) {
-
-//   res.json(req.body.two)
-// }
-// else if (req.body.provider) {
-
-//   res.json(req.body.ten)
-// }
-// else if (req.body.differentsprovider) {
-
-//   res.json(req.body.two)
-// }
-// else if (req.body.detailsdiferentProviders) {
-
-//   res.json(req.body.two)
-// }
-// else if (req.body.remarks) {
-  
-//   res.json(req.body.two)
-// }
+  res.json(req.body.two)
+}
 
 });
 
