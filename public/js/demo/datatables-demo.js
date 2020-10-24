c4274d1058d75458943f12d35d6722dbc142ad79
mendy <maouizerat@gmail.com>
2020-10-25 01:27:21 +0300

    added

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,157 +1,144 @@
 // Call the dataTables jQuery plugin
 var table
 $(document).ready(function () {
-  
+
   // example.php will be used to send the data to the sever database
   $('#dataTable').Tabledit({
     url: 'dailyReport',
     editButton: false,
     deleteButton: true,
     hideIdentifier: true,
-    restoreButton:false,
-  
+    restoreButton: false,
+
 
     columns: {
       identifier: [0, 'id'],
       editable: [
         [1, 'date'], [2, 'credit'], [3, 'debit'], [4, 'apt'],
-       [5, 'name'],[6, 'receipt'],[7, 'forsomeone'],[8, 'details'],
-       [9, 'paymenttype'],[10, 'provider'],[11, 'differentsprovider'],
-       [12, 'detailsdiferentProviders'], [13, 'remarks']
+        [5, 'name'], [6, 'receipt'], [7, 'forsomeone'], [8, 'details'],
+        [9, 'paymenttype'], [10, 'provider'], [11, 'differentsprovider'],
+        [12, 'detailsdiferentProviders'], [13, 'remarks']
       ]
     },
     buttons: {
       edit: {
-          action: 'edit',
-          html:'edit'
+        action: 'edit',
+        html: 'edit'
       },
       delete: {
-            action: 'delete',
-            html:'delete'
+        action: 'delete',
+        html: 'delete'
       },
       save: {
         class: 'btn btn-sm btn-success',
-          html: 'Save'
+        html: 'Save'
       },
       restore: {
         class: 'btn btn-sm btn-warning',
-          html: 'Restore',
-            action: '/'
+        html: 'Restore',
+        action: '/'
       },
       confirm: {
         class: 'btn btn-sm btn-danger comfirme',
-          html: 'Confirm'
+        html: 'Confirm'
       }
     },
 
-    onAjax:function(action,serialize){
+    onAjax: function (action, serialize) {
       var result = urlEncodedStringToObject(serialize)
-     
-      if (result["date"]){
+
+      if (result["date"]) {
         return validatedate(result["date"])
       }
-      else if (result["credit"] || result["debit"] ||result["apt"] ||result["receipt"]){
-       var resultsArr = ["credit" ,"debit","apt","receipt"]
-      var result = resultsArr.forEach(name => {
-        if (result[name]){
-          if (isNaN(result[name])){
-            alert("this is not a good format please write a number")
-            return false
+      else if (result["credit"] || result["debit"] || result["apt"] || result["receipt"]) {
+        var resultsArr = ["credit", "debit", "apt", "receipt"]
+        var result = resultsArr.forEach(name => {
+          if (result[name]) {
+            if (isNaN(result[name])) {
+              alert("this is not a good format please write a number")
+              return false
+            }
+            else {
+              console.log(1)
+              return true
+            }
+          } else {
+            break
           }
-          else{
-            console.log(1)
-           return true
-          }
-        }else {
-          alert("this is not a good format")
-          return false
-        } 
-      })
-      }else{
-        
+        })
+      } else {
+
         return true
       }
-      console.log(5)
-     return false
-    }
   
-    
+    }
+
+
   });
 
   table = $('#dataTable').DataTable();
 });
 
 
-function validatedate(inputText)
-  {
+function validatedate(inputText) {
 
   var dateformat = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
   // Match the date format through regular expression
-  if(inputText.match(dateformat))
-  {
-  
-  //Test which seperator is used '/' or '-'
-  var opera1 = inputText.split('/');
-  var opera2 = inputText.split('-');
-  lopera1 = opera1.length;
-  lopera2 = opera2.length;
-  // Extract the string into month, date and year
-  if (lopera1>1)
-  {
-  var pdate = inputText.split('/');
-  }
-  else if (lopera2>1)
-  {
-  var pdate = inputText.split('-');
-  }
-  var dd = parseInt(pdate[0]);
-  var mm  = parseInt(pdate[1]);
-  var yy = parseInt(pdate[2]);
-  // Create list of days of a month [assume there is no leap year by default]
-  var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-  if (mm==1 || mm>2)
-  {
-  if (dd>ListofDays[mm-1])
-  {
-  alert('Invalid date format!');
-  return false;
-  }
-  }
-  if (mm==2)
-  {
-  var lyear = false;
-  if ( (!(yy % 4) && yy % 100) || !(yy % 400)) 
-  {
-  lyear = true;
-  }
-  if ((lyear==false) && (dd>=29))
-  {
-  alert('Invalid date format!');
-  return false;
-  }
-  if ((lyear==true) && (dd>29))
-  {
-  alert('Invalid date format!');
-  return false;
-  }
-  }
-  }
-  else
-  {
-  alert("Invalid date format!");
-  return false;
-  }
-  }
+  if (inputText.match(dateformat)) {
 
-  function urlEncodedStringToObject (string){
+    //Test which seperator is used '/' or '-'
+    var opera1 = inputText.split('/');
+    var opera2 = inputText.split('-');
+    lopera1 = opera1.length;
+    lopera2 = opera2.length;
+    // Extract the string into month, date and year
+    if (lopera1 > 1) {
+      var pdate = inputText.split('/');
+    }
+    else if (lopera2 > 1) {
+      var pdate = inputText.split('-');
+    }
+    var dd = parseInt(pdate[0]);
+    var mm = parseInt(pdate[1]);
+    var yy = parseInt(pdate[2]);
+    // Create list of days of a month [assume there is no leap year by default]
+    var ListofDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (mm == 1 || mm > 2) {
+      if (dd > ListofDays[mm - 1]) {
+        alert('Invalid date format!');
+        return false;
+      }
+    }
+    if (mm == 2) {
+      var lyear = false;
+      if ((!(yy % 4) && yy % 100) || !(yy % 400)) {
+        lyear = true;
+      }
+      if ((lyear == false) && (dd >= 29)) {
+        alert('Invalid date format!');
+        return false;
+      }
+      if ((lyear == true) && (dd > 29)) {
+        alert('Invalid date format!');
+        return false;
+      }
+    }
+  }
+  else {
+    alert("Invalid date format!");
+    return false;
+  }
+}
 
-    var result ={}
-      var queriesObject = string.split('&')
-      queriesObject.forEach(function(query) {
-        query = query.split('=');
+function urlEncodedStringToObject(string) {
+
+  var result = {}
+  var queriesObject = string.split('&')
+  queriesObject.forEach(function (query) {
+    query = query.split('=');
     result[query[0]] = decodeURIComponent(query[1] || '');
-   
-});
-console.log(result)
-return result
-  }
+
+  });
+  console.log(result)
+  return result
+}
