1119b17dbb8a946f476238eaba2ec1942a30de71
mendy <maouizerat@gmail.com>
2020-10-27 01:20:42 +0200

    added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -168,7 +168,7 @@ else if (req.body.forsomeone) {
   })
 }
 else if (req.body.details) {
-  const SQL = "UPDATE "+dailyReportTable +" SET dailyReportTable = $1 WHERE id = $2;"
+  const SQL = "UPDATE "+dailyReportTable +" SET details = $1 WHERE id = $2;"
   pool.query(SQL, [req.body.details,req.body.id], function (dbError, dbResult) {
     if (dbError) {
       res.json(dbError.stack)
