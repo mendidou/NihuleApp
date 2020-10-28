8e23ac8772c196b9b0f6fbeffceb8ba58a7a4a8d
mendy <maouizerat@gmail.com>
2020-10-28 23:03:51 +0200

    added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -103,7 +103,7 @@ router.post('/updateDailyReport', authMethods.data.authenticateToken, function (
         })
       }
     });
-    if (!Object.keys(err).length == 0) {
+    if (!Object.entries(err).length === 0) {
       console.log(err)
       res.redirect("http://nihuleapi.herokuapp.com/?message=an%20error%20occured%20please%20try%20again")
     }
