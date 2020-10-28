df496bcf4431413a51a7b9fd20eb13b33aaa36e6
mendy <maouizerat@gmail.com>
2020-10-29 00:19:50 +0200

    added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -98,14 +98,15 @@ router.post('/updateDailyReport', authMethods.data.authenticateToken, function (
           pool.query(SQL, [req.body[editReqs[i]], req.body.id], function (dbError, dbResult) {
             if (dbError) {
               err =true
-              console.log(err)
+              console.log( 1 +err)
               return
             } 
           })
         }
       }
-      console.log(err)
+      console.log(2 + err)
       if(err){
+
         res.redirect("http://nihuleapi.herokuapp.com/?message=an%20error%20occured%20please%20try%20again")
       }else{
         res.redirect("/")
